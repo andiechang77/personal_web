1d0a39fe628ae8eaaafd762a3ee8b81b99a52094
Yu-Chien (Andie) Chang <12643937+andiechang77@users.noreply.github.com>
2025-05-16 00:08:09 -0400

    add gallery

---
+++ src/content.config.ts
@@ -0,0 +1,32 @@
+// Import the glob loader
+import { glob } from "astro/loaders";
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `loader` and `schema` for each collection
+const blog = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/blog" }),
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+  }),
+});
+
+const albums = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.yml", base: "./src/albums" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string().optional(),
+      cover: image(),
+    }),
+});
+
+// Export a single `collections` object to register your collection(s)
+export const collections = { albums, blog };
